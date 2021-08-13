ee2a5d82a7023755ed0f26eb5dbf991904921acd
Nathan <dugarnathan@gmail.com>
2021-08-12 20:52:53 -0400

    so rea;

--- repo.sh
+++ repo.sh
@@ -1,29 +1,19 @@
-rm -rf Packages Packages.bz2 Packages.gz Packages.zst Release
-dpkg-scanpackages -m debs > Packages
-bzip2 -k Packages
-gzip -k Packages
-zstd -19 Packages
-cp Base Release
-packages_size=$(ls -l Packages | awk '{print $5,$9}')
-packages_md5=$(md5sum Packages | awk '{print $1}')
-packages_sha256=$(sha256sum Packages | awk '{print $1}')
-packagesbz2_size=$(ls -l Packages.bz2 | awk '{print $5,$9}')
-packagesbz2_md5=$(md5sum Packages.bz2 | awk '{print $1}')
-packagesbz2_sha256=$(sha256sum Packages.bz2 | awk '{print $1}')
-packagesgz_size=$(ls -l Packages.gz | awk '{print $5,$9}')
-packagesgz_md5=$(md5sum Packages.gz | awk '{print $1}')
-packagesgz_sha256=$(sha256sum Packages.gz | awk '{print $1}')
-packageszst_size=$(ls -l Packages.zst | awk '{print $5,$9}')
-packageszst_md5=$(md5sum Packages.zst | awk '{print $1}')
-packageszst_sha256=$(sha256sum Packages.zst | awk '{print $1}')
-echo "MD5Sum:" >> Release
-echo " $packages_md5 $packages_size" >> Release
-echo " $packagesbz2_md5 $packagesbz2_size" >> Release
-echo " $packagesgz_md5 $packagesgz_size" >> Release
-echo " $packageszst_md5 $packageszst_size" >> Release
-echo "SHA256:" >> Release
-echo " $packages_sha256 $packages_size" >> Release
-echo " $packagesbz2_sha256 $packagesbz2_size" >> Release
-echo " $packagesgz_sha256 $packagesgz_size" >> Release
-echo " $packageszst_sha256 $packageszst_size" >> Release
-echo "Done"
+#!/bin/sh
+
+rm -f Packages Packages.xz Packages.zst
+rm -f Contents-iphoneos-arm Contents-iphoneos-arm.xz Contents-iphoneos-arm.zst
+rm -f Release Release.gpg InRelease
+
+apt-ftparchive -c release.conf packages debs > Packages
+xz -9c Packages > Packages.xz
+zstd -9c Packages > Packages.zst
+
+apt-ftparchive -c release.conf contents debs > Contents-iphoneos-arm
+xz -9c Contents-iphoneos-arm > Contents-iphoneos-arm.xz
+zstd -9c Contents-iphoneos-arm > Contents-iphoneos-arm.zst
+
+tmp=$(mktemp)
+apt-ftparchive -c release.conf release . > $tmp
+sed '/debian/d' $tmp > Release
+gpg -abs -u 3E2F176CBEFAA412AA1010CFDDA94DD278B5C270 -o Release.gpg Release
+gpg -abs -u 3E2F176CBEFAA412AA1010CFDDA94DD278B5C270 --clearsign -o InRelease Release
